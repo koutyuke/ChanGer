4174a03294084bc380965ad9aa4182b4a6ab62c6
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-11-24 14:29:26 +0900

    add: setting files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/**/*.{ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        ubuntu: ["Ubuntu","Zen Kaku Gothic Antique"],
+      }
+    },
+  },
+  plugins: [],
+}
