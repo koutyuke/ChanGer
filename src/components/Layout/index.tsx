254fb9998a32a01a7ec80a760a032c4b2bbf3170
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-11-28 22:45:57 +0900

    fix: move files

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import { FC, Fragment } from "react";
+import Header from "../Header";
+import { LayoutProps } from "./type/model";
+
+
+const Layout: FC<LayoutProps> = ({ children }) => (
+  <Fragment key="changer-layout">
+    <Head>
+      <meta charSet="UTF-8" />
+      <title>ChanGer</title>
+      <link rel="icon" href="http://localhost:3000/icon/icon.PNG" />
+    </Head>
+    <main>
+      <div className="flex flex-col">
+        <div className="h-20 w-screen">
+          <Header />
+        </div>
+        <div className="h-[calc(100vh_-_5rem)] w-screen">{children}</div>
+      </div>
+    </main>
+  </Fragment>
+);
+
+export default Layout;
