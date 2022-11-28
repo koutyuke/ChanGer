578f2830a0bf10553d006f1708517b3f5ebba2f9
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-11-29 00:04:22 +0900

    fix: change psth

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { FC, Fragment } from "react";
-import Header from "../Header";
+import Header from "./Header";
 import { LayoutProps } from "./type/model";
 
 
